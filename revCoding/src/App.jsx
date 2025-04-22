555b891ededc66fc2b6ba61b6d094f900d4404c9
Atharv Vedant <avedant2006@gmail.com>
2025-04-22 18:28:59 +0530

    3rd commit
    3rd commit

--- revCoding/src/App.jsx
+++ revCoding/src/App.jsx
@@ -1,15 +1,14 @@
 import './styles/navbar.css';
 import './styles/hero.css';
-import './styles/features.css';
-import './styles/contact.css';
 
 import Navbar from './components/Navbar.jsx';
-
+import Hero from './components/Hero.jsx';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
