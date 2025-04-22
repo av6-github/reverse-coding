3b76f2a004e509b5f76301f74739b1827f0e93c1
Atharv Vedant <avedant2006@gmail.com>
2025-04-22 19:28:06 +0530

    navabr hero
    navabr hero

--- revCoding/src/App.jsx
+++ revCoding/src/App.jsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import Navbarr from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
--- revCoding/src/components/Hero.jsx
+++ revCoding/src/components/Hero.jsx
@@ -2,9 +2,10 @@ import { Container } from 'react-bootstrap';
 
 export default function Hero() {
   return (
-    <Container className="mt-5">
-      <h1 className="display-4">Welcome to Our Project</h1>
-      <p className="lead">Some catchy tagline here.</p>
+    <Container fluid className="mt-5">
+<div style={{ backgroundColor: "turquoise" , padding: "250px", marginTop:"0px"}}>
+  This div has a light blue background.
+</div>
     </Container>
   );
 }
