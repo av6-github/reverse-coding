badb7c49342ba8c4615d94f057d04c9d1b93c75a
Atharv Vedant <avedant2006@gmail.com>
2025-04-22 20:16:35 +0530

    last
    last

--- revCoding/src/components/Hero.jsx
+++ revCoding/src/components/Hero.jsx
@@ -1,11 +1,36 @@
 import { Container } from 'react-bootstrap';
+import heroimg from '../assets/heroimg.png'
+import midone from '../assets/midone.png'
+import midtown from '../assets/midtown.png'
+import bottomone from '../assets/bottomone.png'
 
 export default function Hero() {
   return (
     <Container fluid className="mt-5">
-<div style={{ backgroundColor: "turquoise" , padding: "250px", marginTop:"0px"}}>
-  This div has a light blue background.
-</div>
+    <div style={{ padding: "250px", margin:"0px", display: "flex", justifyContent: "center", position: "relative", bottom: "200px"}}>
+        <img src={heroimg} style = {{height: "2000%", width: "2000%"}}alt="img" />
+    </div>
+
+
+        <div style={{ padding: "250px", margin:"0px", display: "flex", justifyContent: "center", position: "relative", bottom: "200px"}}>
+            <p style={{textAlign: "center", fontSize: "50px", fontWeight: "bold", color: "white"}}>Manage your entire process, </p>
+            <p>from sourcing to employee onboarding</p>
+            <p>and management</p>
+            <img src={midone} style = {{height: "2000%", width: "2000%"}}alt="img" />
+        </div>
+
+
+        <div style={{ margin:"0px", display: "flex", justifyContent: "center"}}>
+            <img src={midtown} style = {{height: "2000%", width: "2000%"}}alt="img" />
+        </div>
+
+        <div style={{ margin:"0px", display: "flex", justifyContent: "center"}}>
+            <img src={bottomone} style = {{height: "2000%", width: "2000%"}}alt="img" />
+        </div>
+        
+
+
+
     </Container>
   );
 }
--- revCoding/src/components/Navbar.jsx
+++ revCoding/src/components/Navbar.jsx
@@ -9,17 +9,25 @@ export default function Navbarr() {
   return (
     <Navbar style={{ backgroundColor: "turquoise"}} expand="lg" className="bg-turquoise">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">workable</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown title="Product" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Product</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#action2">Pricing</Nav.Link>
+            <NavDropdown title="Our customers" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -29,18 +37,13 @@ export default function Navbarr() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+            <Nav.Link href="#">
+              Resources
             </Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
+            
+             <Button variant="outline-success">Request demo</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
